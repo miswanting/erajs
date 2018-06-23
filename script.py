aefb26b30e5140e2f3506171915dc719db999add
何雨航 <453542772@qq.com>
2018-06-23 13:56:04 +0800

    Go

--- script.py
+++ script.py
@@ -1,11 +1,12 @@
 # coding:utf-8
-import src.game as g
+import engine.game as g
+import src.lib_base as base
 
 
 def intro():
     g.new_page()
     g.pl('请选择主角的创建方式')
-    g.pl()  # 空单独一行
+    g.pl()
     g.plcmd("使用游戏默认初始角色", default_start)
 
 
@@ -14,11 +15,12 @@ def load():
 
 
 def default_start():
-    pass
+    g.data['人物库'] = [base.default_character()]
+    print(g.data)
 
 
 g.init()
 g.pl('EraLife')
-g.pl()  # 空单独一行
+g.pl()
 g.plcmd("开始游戏", g.goto, intro)
 g.plcmd("读取游戏", g.goto, load)
