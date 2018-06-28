0b24a68dcd50d6dfd4441cd074844585c35e7254
何雨航 <453542772@qq.com>
2018-06-28 13:30:05 +0800

    加入行对齐

--- engine/era.js
+++ engine/era.js
@@ -51,9 +51,12 @@ function newLine() {
         $(".current-line").removeClass("current-line")
     }
     let newLine = $("<p></p>")
-    newLine.addClass("my-0 current-line")
+    newLine.addClass("my-0 clearfix current-line")
     newLine.css("word-break", "break-all")
     $(".current-page").append(newLine)
+    let leftAlign = $("<p></p>")
+    let rightAlign = $("<p></p>")
+    $(".current-line").append(newLine)
 }
 ipcRenderer.on('package', (event, data) => {
     // console.log(data)
