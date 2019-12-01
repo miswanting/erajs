8d1b0c5325c367f3f22c000ef28cfd329fd850d4
(ง •_•)ง <miswanting@users.noreply.github.com>
2019-12-01 23:51:13 +0800

    Update Net.py

--- Core/BackEnd/erajs/Managers/Net.py
+++ Core/BackEnd/erajs/Managers/Net.py
@@ -4,10 +4,11 @@ from flask_socketio import SocketIO
 from ..Prototypes.Singleton import Singleton
 
 
-class NetManager(Singleton):
+class NetManager(Singleton, Namespace):
     def bind(self, address):
         app = Flask(__name__)
         sio = SocketIO(app)
+        sio.on_event('connect', self.on_connect)
 
-    def connect(self, address):
+    def on_connect(self, data):
         pass
