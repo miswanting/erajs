480166706079dcdd21ee5300c45782ab7446f6e0
(ง •_•)ง <miswanting@users.noreply.github.com>
2020-04-12 20:31:22 +0800

    ui更新

--- Pure/BackEnd/test.py
+++ Pure/BackEnd/test.py
@@ -1,21 +1,27 @@
+import os
+import sys
+
 import erajs.api as a
 
 
 def cover():
     a.page()
-    a.h('test')
+    a.h('封面')
     a.t()
-    a.t('test')
-    a.b('test', aaa)
-    a.l('test')
+    a.b('进入', aaa)
 
 
 def aaa():
     a.page()
+    a.h('封面')
+    a.t()
     a.b('test', aaa)
+    print(a.data)
 
 
 if __name__ == "__main__":
+    print(os.getcwd())
     a.init()
+    print(os.getcwd())
     a.entry(cover)
     a.start()
