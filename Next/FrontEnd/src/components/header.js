95c46e83870e835677d74a5d95f6667fdf3e4075
(ง •_•)ง <miswanting@users.noreply.github.com>
2020-06-04 22:14:27 +0800

    Implement Header Menu

--- Next/FrontEnd/src/components/header.js
+++ Next/FrontEnd/src/components/header.js
@@ -23,18 +23,69 @@ module.exports = function Header(props) {
 function MenuBar(props) {
     return (
         React.createElement('span', { className: 'menu-bar' },
-            React.createElement(
-                'span',
-                { className: 'menu' },
-                '≡'  // ☰
-            ),
-            React.createElement(
-                'span',
-                { className: 'menu-add' },
-                '+'  // ＋
-            )
+            React.createElement(Menu),
+            React.createElement(MenuAdd)
         )
     )
+}
+
+function Menu(props) {
+    const [show, setShow] = React.useState(false)
+    showList = [React.createElement(
+        'span',
+        {
+            className: 'menu-button',
+            onClick: () => { setShow(!show) }
+        },
+        '≡'  // ☰
+    )]
+    if (show) {
+        showList.push(
+            React.createElement(
+                'div', { className: 'menu-list' }, [
+                React.createElement('div', { className: 'menu-item' }, '文件'),
+                React.createElement('div', { className: 'menu-item' }, '编辑'),
+                React.createElement('div', { className: 'menu-item' }, '窗口'),
+                React.createElement('div', { className: 'menu-item' }, '帮助')
+            ]
+            ))
+    }
+    return (
+        React.createElement('span', { className: 'menu' }, showList)
+    )
+}
+
+function MenuAdd(props) {
+    const [show, setShow] = React.useState(false)
+    showList = [React.createElement(
+        'span',
+        {
+            className: 'menu-add-button',
+            onClick: () => { setShow(!show) }
+        },
+        '+'  // ＋
+    )]
+    if (show) {
+        showList.push(
+            React.createElement(
+                'div', { className: 'menu-list' }, [
+                React.createElement('div', { className: 'menu-item' }, '文件'),
+                React.createElement('div', { className: 'menu-item' }, '编辑'),
+                React.createElement('div', { className: 'menu-item' }, '窗口'),
+                React.createElement('div', { className: 'menu-item' }, '帮助')
+            ]
+            ))
+    }
+    return (
+        React.createElement('span', { className: 'menu' }, showList)
+    )
+    // return (
+    //     React.createElement(
+    //         'span',
+    //         { className: 'menu-add' },
+    //         '+'  // ＋
+    //     )
+    // )
 }
 function Title(props) {
     return (
@@ -47,9 +98,9 @@ function Title(props) {
 }
 function WindowOperatorBar(props) {
     const click = (type) => {
-        console.log(123);
+        // console.log(123);
         if (type == 'min') {
-            // remote.getCurrentWindow().unminimize()
+            remote.getCurrentWindow().unminimize()
         } else if (type == 'max') {
             if (remote.getCurrentWindow().isMaximized()) {
                 remote.getCurrentWindow().unmaximize()
