7d7687dd7fb80f7a9758ba82aab3eca02459c01f
(ง •_•)ง <miswanting@users.noreply.github.com>
2020-06-07 00:43:34 +0800

    Add Toast

--- Next/FrontEnd/src/ui/game.js
+++ Next/FrontEnd/src/ui/game.js
@@ -1,4 +1,5 @@
 const React = require('../../node_modules/react')
+const anime = require('../../node_modules/animejs')
 const Header = require('../components/header')
 const Container = require('../components/container')
 const Footer = require('../components/footer')
@@ -8,8 +9,63 @@ module.exports = function Game(props) {
             'div',
             { className: 'window' },
             React.createElement(Header, props),
+            React.createElement(ToastSystem, props),
             React.createElement(Container, props),
             React.createElement(Footer, props)
+        )
+    )
+}
+function ToastSystem(props) {
+    return (
+        React.createElement(
+            'div',
+            { className: 'toast-bar' },
+            React.createElement(
+                'div',
+                { className: 'toast-anchor' },
+                React.createElement(ToastList)
+            )
+        )
+    )
+}
+function ToastList(props) {
+    return (
+        React.createElement(
+            'div',
+            { className: 'toast-list' },
+            [
+                React.createElement(ToastItem),
+                // React.createElement(ToastItem)
+            ]
+        )
+    )
+}
+function ToastItem(props) {
+    // const [show, setShow] = React.useState(false)
+    React.useEffect(() => {
+        anime({
+            targets: '.toast-item',
+            translateX: [150, 0],
+            delay: 1000,
+            duration: 4000,
+            easing: 'easeOutCubic',
+            complete: () => {
+                anime({
+                    targets: '.toast-item',
+                    translateX: [0, 150],
+                    delay: 1000,
+                    duration: 4000,
+                    easing: 'easeOutCubic'
+                });
+            }
+        });
+
+    })
+    return (
+        React.createElement(
+            'div',
+            { className: 'toast-item' },
+            '弹出消息'
         )
     )
 }
