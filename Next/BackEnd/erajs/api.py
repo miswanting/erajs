5c4b8f24222329f04c34bff4256b28b949d9ec75
(ง •_•)ง <miswanting@users.noreply.github.com>
2020-09-24 07:51:32 +0800

    Update api.py

--- Next/BackEnd/erajs/api.py
+++ Next/BackEnd/erajs/api.py
@@ -135,22 +135,22 @@ def cls(num: int = 0) -> None:
 
 
 ########## 块 ##########
-def mode(type: Optional[str] = 'line', *arg: Any, **kw: Any):
+def mode(type: Optional[str] = 'line', *arg: Any, **kw: Any) -> None:
     """
     # 设置新增控件排版模式
     """
     m.mode(type, *arg, **kw)
 
 
-def divider(text: Optional[str] = None, style: Optional[Dict[str, str]] = None):
+def divider(text: Optional[str] = None, style: Optional[Dict[str, str]] = None) -> object:
     """
     # 插入块级控件：分割线
     """
-    m.divider(text, style)
+    return m.divider(text, style)
 
 
 ########## 行 ##########
-def heading(text: Optional[str] = None, rank: int = 1, style: Optional[Dict[str, str]] = None):
+def heading(text: Optional[str] = None, rank: int = 1, style: Optional[Dict[str, str]] = None) -> object:
     """
     # 插入行内控件：标题
     """
@@ -160,7 +160,7 @@ def heading(text: Optional[str] = None, rank: int = 1, style: Optional[Dict[str,
 h = heading
 
 
-def text(text: Optional[str] = None, wait: bool = False, style: Optional[Dict[str, str]] = None):
+def text(text: Optional[str] = None, wait: bool = False, style: Optional[Dict[str, str]] = None) -> object:
     """
     # 插入行内控件：文本
     """
@@ -170,7 +170,7 @@ def text(text: Optional[str] = None, wait: bool = False, style: Optional[Dict[st
 t = text
 
 
-def button(text: Optional[str] = None, callback: Optional[Callable[[], None]] = None, *arg: Any, **kw: Any):
+def button(text: Optional[str] = None, callback: Optional[Callable[[], None]] = None, *arg: Any, **kw: Any) -> object:
     """
     # 插入行内控件：按钮
     """
@@ -180,7 +180,7 @@ def button(text: Optional[str] = None, callback: Optional[Callable[[], None]] = 
 b = button
 
 
-def link(text: Optional[str] = None, callback: Optional[Callable[[], None]] = None, style: Optional[Dict[str, str]] = None, *arg: Any, **kw: Any):
+def link(text: Optional[str] = None, callback: Optional[Callable[[], None]] = None, style: Optional[Dict[str, str]] = None, *arg: Any, **kw: Any) -> object:
     """
     # 插入行内控件：链接
     """
@@ -190,42 +190,42 @@ def link(text: Optional[str] = None, callback: Optional[Callable[[], None]] = No
 l = link
 
 
-def progress(now: int = 0, max: int = 100, style: Optional[Dict[str, str]] = None):
+def progress(now: int = 0, max: int = 100, style: Optional[Dict[str, str]] = None) -> object:
     """
     # 插入行内控件：进度条
     """
     return m.progress(now, max, style)
 
 
-def rate(now: int = 0, max: int = 5, callback: Optional[Callable[[int], None]] = None, style: Optional[Dict[str, str]] = None):
+def rate(now: int = 0, max: int = 5, callback: Optional[Callable[[int], None]] = None, style: Optional[Dict[str, str]] = None) -> object:
     """
     # 插入行内控件：评分
     """
     return m.rate(now, max, callback, style)
 
 
-def check(text: Optional[str] = None, callback: Optional[Callable[[bool], None]] = None, default: bool = False, style: Optional[Dict[str, str]] = None):
+def check(text: Optional[str] = None, callback: Optional[Callable[[bool], None]] = None, default: bool = False, style: Optional[Dict[str, str]] = None) -> object:
     """
     # 插入行内控件：多选
     """
     return m.check(text, callback, default, style)
 
 
-def radio(text_list: List[str], callback: Optional[Callable[[int], None]] = None, default_index: int = 0, style: Optional[Dict[str, str]] = None):
+def radio(text_list: List[str], callback: Optional[Callable[[int], None]] = None, default_index: int = 0, style: Optional[Dict[str, str]] = None) -> object:
     """
     # 插入行内控件：单选
     """
     return m.radio(text_list, callback, default_index, style)
 
 
-def input(callback: Optional[Callable[[str], None]] = None, default: str = '', is_area: bool = False, placeholder: str = '', style: Optional[Dict[str, str]] = None):
+def input(callback: Optional[Callable[[str], None]] = None, default: str = '', is_area: bool = False, placeholder: str = '', style: Optional[Dict[str, str]] = None) -> object:
     """
     # 插入行内控件：输入框
     """
     return m.input(callback, default, is_area, placeholder, style)
 
 
-def dropdown(text_list: Optional[List[str]] = None, callback: Optional[Callable[[int], None]] = None, default_index: int = 0, search: bool = False, multiple: bool = False, placeholder: str = '', allowAdditions: bool = False, style: Optional[Dict[str, str]] = None):
+def dropdown(text_list: Optional[List[str]] = None, callback: Optional[Callable[[int], None]] = None, default_index: int = 0, search: bool = False, multiple: bool = False, placeholder: str = '', allowAdditions: bool = False, style: Optional[Dict[str, str]] = None) -> object:
     """
     # 插入行内控件：下拉列表
     """
@@ -234,14 +234,14 @@ def dropdown(text_list: Optional[List[str]] = None, callback: Optional[Callable[
 
 
 ########## 样式管理 ##########
-def set_style(widget: Callable[[], Any], style: Dict[str, str]):
+def set_style(widget: Callable[[], Any], style: Dict[str, str]) -> None:
     """
     # 设置样式
     """
     m.set_style(widget, style)
 
 
-def reset_style(widget: Callable[[], Any]):
+def reset_style(widget: Callable[[], Any]) -> None:
     """
     # 重置样式
     """
@@ -249,99 +249,99 @@ def reset_style(widget: Callable[[], Any]):
 
 
 ########## 界面逻辑 ##########
-def goto(ui_func: Callable[[], Any], *arg: Any, **kw: Any):
+def goto(ui_func: Callable[[], Any], *arg: Any, **kw: Any) -> None:
     """
     # 将界面节点添加到界面栈的末尾，并触发
     """
     m.goto(ui_func, *arg, **kw)
 
 
-def back(num: int = 1, *arg: Any, **kw: Any):
+def back(num: int = 1, *arg: Any, **kw: Any) -> None:
     """
     # 从界面栈删除n（默认为1）个界面节点，并触发删除之后的末尾节点
     """
     m.back(num, *arg: Any, **kw: Any)
 
 
-def repeat(*arg: Any, **kw: Any):
+def repeat(*arg: Any, **kw: Any) -> None:
     """
     # 不对界面栈进行修改，并触发末尾节点
     """
     pass
 
 
-def clear(num: int = 0):
+def clear(num: int = 0) -> None:
     """
     # 清空界面栈
     """
     pass
 
 
-def insert(ui_func: Callable[[], Any], *arg: Any, **kw: Any):
+def insert(ui_func: Callable[[], Any], *arg: Any, **kw: Any) -> None:
     """
     # 将界面节点添加到界面栈的末尾，但不触发
     """
     pass
 
 
-def get_gui_stack():
+def get_gui_stack() -> List[str]:
     """
     # 返回界面栈，以界面名称列表的格式
     """
-    pass
+    return m.get_gui_stack()
 
 
 ########## 数据管理 ##########
-def cfg(dot_path: Optional[str] = None):
+def cfg(dot_path: Optional[str] = None) -> Any:
     """
     # 返回设置数据
     """
     return m.cfg(dot_path)
 
 
-def dat(dot_path: Optional[str] = None):
+def dat(dot_path: Optional[str] = None) -> Any:
     """
     # 返回静态数据
     """
     return m.dat(dot_path)
 
 
-def sav(dot_path: Optional[str] = None):
+def sav(dot_path: Optional[str] = None) -> Any:
     """
     # 返回存档数据
     """
     return m.sav(dot_path)
 
 
-def tmp(key: Optional[str] = None) -> Dict[Any, Any]:
+def tmp(key: Optional[str] = None) -> Any:
     """
     # 返回临时数据
     """
     return m.tmp(key)
 
 
-def write_cfg(dot_path: Optional[str] = None, ext: str = 'ini'):
+def write_cfg(dot_path: Optional[str] = None, ext: str = 'ini') -> None:
     """
     # 转储设置数据
     """
     return m.write_cfg(dot_path, ext)
 
 
-def write_dat(dot_path: Optional[str] = None, ext: str = 'ini'):
+def write_dat(dot_path: Optional[str] = None, ext: str = 'ini') -> None:
     """
     # 转储静态数据
     """
     return m.write_dat(dot_path, ext)
 
 
-def write_sav(filename_without_ext: str = None):
+def write_sav(filename_without_ext: Optional[str] = None) -> None:
     """
     # 转储存档数据
     """
     m.write_sav(filename_without_ext)
 
 
-def read_sav(filename_without_ext: str = None):
+def read_sav(filename_without_ext: Optional[str] = None):
     """
     # 转储存档数据
     """
@@ -349,28 +349,28 @@ def read_sav(filename_without_ext: str = None):
 
 
 ########## 事件 ##########
-def on(event_name: str, listener: Callable[[], Any]):
+def on(event_name: str, listener: Callable[[], Any]) -> None:
     """
     # 注册侦听器
     """
     pass
 
 
-def once(event_name: str, listener: Callable[[], Any]):
+def once(event_name: str, listener: Callable[[], Any]) -> None:
     """
     # 注册一次性侦听器
     """
     pass
 
 
-def off(event_name: str, listener: Callable[[], Any]):
+def off(event_name: str, listener: Callable[[], Any]) -> None:
     """
     # 注销侦听器
     """
     pass
 
 
-def emit(event_name: str, *arg: Any, **kw: Any):
+def emit(event_name: str, *arg: Any, **kw: Any) -> None:
     """
     # 发布事件
     """
@@ -378,7 +378,7 @@ def emit(event_name: str, *arg: Any, **kw: Any):
 
 
 ########## 实验性 ##########
-def dangerously_get_engine_core():
+def dangerously_get_engine_core() -> object:
     """
     # 获取引擎核心（危险）
     """
@@ -386,7 +386,7 @@ def dangerously_get_engine_core():
 
 
 ########## 工具 ##########
-def random_hash(level: int = 4):
+def random_hash(level: int = 4) -> str:
     """
     # 获取哈希随机值
     """
@@ -394,14 +394,14 @@ def random_hash(level: int = 4):
 
 
 ########## 预设控件 ##########
-def show_save_to_save():
+def show_save_to_save() -> None:
     """
     # 插入块级复合控件：存档保存列表（单页）
     """
     pass
 
 
-def show_save_to_load():
+def show_save_to_load() -> None:
     """
     # 插入块级复合控件：存档加载列表（单页）
     """
