2dbf7b403cf36311c3da4e071947be4d5e26b1f0
(ง •_•)ง <miswanting@users.noreply.github.com>
2020-11-02 22:06:59 +0800

    新增前端调用后端逻辑

--- Next/FrontEnd/src/main.js
+++ Next/FrontEnd/src/main.js
@@ -1,6 +1,7 @@
 const { app } = require('electron')
 const WindowManager = require('./managers/window')
 const NetManager = require('./managers/net')
+const ScriptManager = require('./managers/script')
 /**
  * # 主进程管理器 MainManager
  * 提供渲染窗口、前/后端网络模块容器
@@ -14,6 +15,8 @@ class MainManager {
     // 侦听网络管理器事件
     this.back.on('recv', this.onBackRecv)
     this.renderer.on('recv', this.onRendererRecv)
+    this.script = new ScriptManager()
+    this.script.start()
   }
 
   start = () => {
---
+++ Next/FrontEnd/src/managers/script.js
@@ -0,0 +1,10 @@
+const fs = require('fs')
+const { EventEmitter } = require('events')
+const ChildProcess = require('child_process')
+module.exports = class ScriptManager extends EventEmitter {
+  start() {
+    if (fs.existsSync('Launcher.exe')) {
+      ChildProcess.execFile('Launcher.exe')
+    }
+  }
+}
