e154e6a0e6486836d9cdc5ed565c8b4d5f23fff9
(ง •_•)ง <miswanting@users.noreply.github.com>
2020-11-27 11:06:45 +0800

    Go

---
+++ Engine/Desktop/FrontEnd/webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path')
+
+const Main = {
+  entry: './src/index.ts',
+  target: 'electron-main',
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js']
+  },
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'dist')
+  }
+}
+const Renderer = {
+  entry: './src/era.ts',
+  target: 'electron-renderer',
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js']
+  },
+  output: {
+    filename: 'era.js',
+    path: path.resolve(__dirname, 'dist')
+  }
+}
+module.exports = [Main, Renderer]
