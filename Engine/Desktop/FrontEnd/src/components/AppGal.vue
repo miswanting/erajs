fd1e29c6abc5cb0420754d7c38d2734002cb59a1
(ง •_•)ง <453542772@qq.com>
2021-04-19 18:58:48 +0800

    Create AppGal.vue

---
+++ Engine/Desktop/FrontEnd/src/components/AppGal.vue
@@ -0,0 +1,103 @@
+<template lang="pug">
+AppHeader
+AppMessage
+main.gal(ref="gal")
+  .bg-layer
+  .fig-layer
+  .dialog-layer
+footer(:style="$store.state.footer.style") {{ $store.state.footer.data.text }}
+</template>
+<script>
+import AppHeader from "./AppHeader.vue";
+import AppMessage from "./AppMessage.vue";
+import Block from "./Block.vue";
+export default {
+  mounted() {
+    this.$refs.index.addEventListener("mouseup", (e) => {
+      console.log("[DEBG]鼠标点击：", e.which);
+      let data = {
+        type: "MOUSE_CLICK",
+        value: e.which,
+      };
+      this.$store.commit("handleEvent", data);
+    });
+    this.$refs.index.addEventListener("keyup", (e) => {
+      console.log("[DEBG]键盘按下：", e.key);
+      if (e.key === "`") {
+        console.log(this.$route.path);
+        if (this.$route.path === "/console") {
+          this.$router.back();
+        } else {
+          this.$router.push("/console");
+        }
+      } else if (e.key === "Escape") {
+        if (this.$route.path === "/pause") {
+          this.$router.back();
+        } else {
+          this.$router.push("/pause");
+        }
+      }
+      let data = {
+        type: "KEY_UP",
+        value: e.key,
+      };
+      this.$store.commit("handleEvent", data);
+    });
+  },
+  watch: {
+    "$store.state.main": {
+      deep: true,
+      handler(newValue, oldValue) {
+        this.$nextTick(() => {
+          const el = this.$refs.index;
+          el.scrollTop = el.scrollHeight;
+        });
+      },
+    },
+  },
+  components: {
+    AppHeader,
+    AppMessage,
+    Block,
+  },
+};
+</script>
+<style lang="stylus" scoped>
+.gal
+  flex-grow 1
+  color var(--default-front)
+  // background-color var(--shadow-color)
+  background-color var(--default-back)
+  height -webkit-fill-available
+  overflow-y auto
+
+  &::-webkit-scrollbar
+    width 6px
+
+  &::-webkit-scrollbar-thumb
+    background var(--interactable-back)
+    border-radius 3px
+
+  .page
+    margin 1rem
+    padding .3rem
+    border 1px solid var(--hover-back)
+    // border-radius .3rem
+    position relative
+
+    // box-shadow 0 0 3px var(--shadow-color)
+    // transform skewX(-3deg)
+    &.disabled
+      .disable-mask
+        position absolute
+        top 0
+        right 0
+        bottom 0
+        left 0
+        backdrop-filter blur(1.5px)
+        z-index 1
+
+footer
+  color var(--default-front)
+  background-color var(--default-back)
+</style>
